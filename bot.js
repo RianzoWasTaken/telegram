ce3b103947e0ceb362b4fa12c2b11c6a50d40e7a
ITS Me Rianzo <132869464+RianzoWasTaken@users.noreply.github.com>
2023-10-06 13:50:11 +0800

    bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,49 @@
 const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios');
+const ytdl = require('ytdl-core');
+const fs = require('fs');
 const token = '6356132155:AAGuHxZ97zxaPxfW4MwiF-iqfSOlaEsTiW4'; // Ganti dengan token API bot Anda
 const bot = new TelegramBot(token, { polling: true });
 
-// Menangani perintah /download
-bot.onText(/\/download (.+)/, async (msg, match) => {
+// Menangani perintah /downloadvideo
+bot.onText(/\/downloadvideo (.+)/, async (msg, match) => {
   const chatId = msg.chat.id;
-  const videoUrl = match[1]; // Mengambil URL video dari perintah pengguna
+  const youtubeUrl = match[1]; // Mengambil URL YouTube dari perintah pengguna
 
   try {
-    // Mengunduh video dari URL menggunakan Axios
-    const response = await axios.get(videoUrl, { responseType: 'stream' });
+    // Mendapatkan info video dari URL YouTube
+    const info = await ytdl.getInfo(youtubeUrl);
 
-    // Mengirim video ke pengguna
-    bot.sendVideo(chatId, response.data, { caption: 'Ini video yang Anda unduh.' });
+    // Mendapatkan format video MP4 terbaik
+    const format = ytdl.chooseFormat(info.formats, { quality: 'highestvideo' });
+
+    // Mengunduh video dari URL YouTube
+    const videoStream = ytdl(youtubeUrl, { format });
+
+    // Menyimpan video di server
+    const videoFileName = 'video.mp4';
+    const videoPath = `../videos/${videoFileName}`;
+    const writeStream = fs.createWriteStream(videoPath);
+    videoStream.pipe(writeStream);
+
+    videoStream.on('end', () => {
+      // Mengirim video ke pengguna
+      bot.sendVideo(chatId, videoPath, { caption: 'Ini video MP4 yang Anda unduh dari YouTube.' });
+    });
   } catch (error) {
     console.error(error);
-    bot.sendMessage(chatId, 'Maaf, ada masalah saat mengunduh video.');
+    bot.sendMessage(chatId, 'Maaf, ada masalah saat mengunduh video dari YouTube.');
   }
 });
 
 // Menangani perintah /start
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Selamat datang! Kirimkan perintah /download [URL_VIDEO] untuk mengunduh video MP4.');
+  bot.sendMessage(chatId, 'Selamat datang! Kirimkan perintah /downloadvideo [URL_YOUTUBE] untuk mengunduh video MP4 dari YouTube.');
 });
 
 // Menangani pesan yang tidak sesuai perintah
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Perintah tidak valid. Kirimkan perintah /download [URL_VIDEO] untuk mengunduh video MP4.');
+  bot.sendMessage(chatId, 'Perintah tidak valid. Kirimkan perintah /downloadvideo [URL_YOUTUBE] untuk mengunduh video MP4 dari YouTube.');
 });
-  
+                                             
